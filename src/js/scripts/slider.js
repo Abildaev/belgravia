75fb86eb345b4b790ddba8f42db3eca679bd8e95
Abildaev Dastan <abildaev1996@gmail.com>
2020-04-26 01:21:38 +0600

    dsld

---
+++ src/js/scripts/slider.js
@@ -0,0 +1,30 @@
+     var swiper = new Swiper('.entry-js', {
+       navigation: {
+         nextEl: '.next',
+         prevEl: '.prev',
+       },
+       autoplay: {
+         delay: 2500,
+         disableOnInteraction: false,
+       },
+     });
+
+     var swiper = new Swiper('.gallery-js', {
+       simulateTouch: false,
+       pagination: {
+         el: '.swiper-pagination',
+         clickable: true,
+       },
+     });
+
+
+     var swiper = new Swiper('.plan-js', {
+       slidesPerView: 1,
+       navigation: {
+         nextEl: '.next',
+         prevEl: '.prev',
+       }
+     });
+
+
+     
